7353e555d5ca4b22cf3dbf82ad6fcad272c4ac48
Devita Halim <devitarahmad@student.unimelb.edu.au>
2022-08-22 23:17:45 +1000

    Edit the function (simulation)

---
+++ SimulationOfGMM.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import PdfGaussian as pdf
+import numpy as np
+from scipy.stats import multivariate_normal
+
+def SimulateGMM(n, mean1, sig1, mean2, sig2):
+    X1=np.random.normal(mean1,np.sqrt(sig1),n)
+    X2=np.random.normal(mean2,np.sqrt(sig2),n)
+    X=np.array(list(X1)+list(X2))
+    #gmm_datapoints=np.linspace(np.min(X),np.max(X),100)
+    #plt.figure(figsize=(8,5))
+
+    #plt.scatter(X, [0.005] * len(X), color='mediumslateblue', s=15, marker="|", label="Data points")
+
+    #plt.plot(gmm_datapoints, pdf(gmm_datapoints, mean1, sig1), color='black', label="True pdf")
+    #plt.plot(gmm_datapoints, pdf(gmm_datapoints, mean2, sig2), color='black')
+    
+    #plt.xlabel=("x")
+    #plt.ylabel=("Probability Density Function (PDF)")
+    #plt.legend()
+    #plt.show()
+    print(X)
+    print("hi")
+
+SimulateGMM(n=100,mean1=10,sig1=4,mean2=-5,sig2=2)
