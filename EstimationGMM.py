f56c0518f828266f4884a7f6e07160f98599a73b
Devita Halim <devitarahmad@student.unimelb.edu.au>
2022-08-24 15:14:16 +1000

    Edit the estimation and visualization function

--- EstimationGMM.py
+++ EstimationGMM.py
@@ -1,14 +1,19 @@
+from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 from PdfGaussian import GaussianPDF
+from SimulationOfGMM import SimulateGMM
 
 def GaussianEM(X,k:int,n_iteration:int):
     means=np.random.choice(X, k)
     variances=np.random.random_sample(size=k)
     weights=np.ones((k))/k
+    X=np.array(X)
 
     epsilon=1e-9
+    means_all=[]
+    var_all=[]
 
     for iteration in range(n_iteration):
         
@@ -27,9 +32,23 @@ def GaussianEM(X,k:int,n_iteration:int):
             l.append((likelh[j] * weights[j]) / (np.sum([likelh[i] * weights[i] for i in range(k)], axis=0)+epsilon))
   
             #Update the parameters
-            means[j] = np.sum(l[j] * X) / (np.sum(l[j]+epsilon))
-            variances[j] = np.sum(l[j] * np.square(X - means[j])) / (np.sum(l[j]+epsilon))
+            means_all.append(np.sum(l[j] * X) / (np.sum(l[j]+epsilon)))
+            var_all.append(np.sum(l[j] * np.square(X - means[j])) / (np.sum(l[j]+epsilon)))
             weights[j] = np.mean(l[j])
-        
-        return(means,variances)
+    
+    means_all=np.array(means_all)
+    var_all=np.array(var_all)
 
+    means_one=means_all[::2]
+    means_two=means_all[1::2]
+
+    means_total=np.column_stack((means_one,means_two))
+
+    var_one=var_all[::2]
+    var_two=var_all[1::2]
+
+    var_total=np.column_stack((var_one, var_two))
+    
+    return(means_total,var_total,n_iteration)
+
+GaussianEM(SimulateGMM(100,10,4,-5,3),2,15)
---
+++ VisualizationGMM.py
@@ -0,0 +1,24 @@
+from statistics import variance
+import matplotlib.pyplot as plt
+import numpy as np
+from PdfGaussian import GaussianPDF
+from SimulationOfGMM import SimulateGMM
+from EstimationGMM import GaussianEM
+
+def PlotGMM(X,means,variances,n_iteration,plot:int):
+    gmm_datapoints=np.linspace(np.min(X),np.max(X),100)
+    for iteration in range(n_iteration):
+        if iteration%plot==0:
+            plt.figure(figsize=(8,5))
+            plt.scatter(X, [0.005] * len(X), color='mediumslateblue', s=15, marker="|", label="Data points")
+            
+            #Plot the estimated pdf
+            plt.plot(gmm_datapoints, GaussianPDF(gmm_datapoints, means[0], variances[0]), color='red', label="Distribution 1")
+            plt.plot(gmm_datapoints, GaussianPDF(gmm_datapoints, means[1], variances[1]), color='green', label="Distribution 2")
+            
+            #Set the x and y label
+            plt.xlabel=("x")
+            plt.ylabel=("Probability Density Function (PDF)")
+            plt.legend(loc="upper left")
+
+            plt.show()
